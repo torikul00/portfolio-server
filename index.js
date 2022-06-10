9c59bd8d3d6b6d9db7b11d2175bc6c677fb18aff
Torikul Islam <toriqulislam142@gmail.com>
2022-06-10 09:29:14 -0700

    git test

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+
 
 app.use(express.json())
 app.use(cors())
@@ -21,12 +22,15 @@ async function run() {
         const projects = projectsCollection.find()
         const result = await projects.toArray()
         res.send(result)
-
+    })
+    app.get('/project/:id', async (req, res) => {
+        const id = req.params.id
+        const query = {_id:ObjectId(id)}
+        const project = await projectsCollection.findOne(query)
+        res.send(project)
     })
 }
 run()
-
-
 app.get('/', (req, res) => {
     res.send('Portfolio server is running')
 })
